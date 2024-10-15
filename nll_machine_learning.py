3048f86f42f2095b4a6ee65856a5a91715993aeb
r-stava <shriva23@pnw.edu>
2024-10-15 13:23:31 -0500

    Update nll_machine_learning.py

--- nll_machine_learning.py
+++ nll_machine_learning.py
@@ -138,7 +138,7 @@ plt.grid(True)
 plt.legend()
 plt.show()
 
-pip install pmdarima
+
 
 # Step 1: Prepare the vacancy time series data
 
@@ -260,7 +260,6 @@ print(f"Vacancy Rate Prediction in 10 years: {forecasts[10]:.2f}")
 
 """Time Series Algorithms | Prophet | Home Value Rate and Rental Rate"""
 
-pip install prophet
 
 # Step 2.1: Prepare rental rates data for Prophet
 # Transpose to get dates as rows instead of columns
@@ -430,9 +429,6 @@ print(f"\nRental Rate Prediction Accuracy: MSE={rental_mse}, MAE={rental_mae}")
 print(f"Home Value Rate Prediction Accuracy: MSE={home_value_mse}, MAE={home_value_mae}")
 
 """Dashboard | Printing the predictions for vacancy rate, home value rate and rental rate"""
-
-# Step 1: Install necessary libraries if not already installed
-pip install streamlit matplotlib prophet pmdarima
 
 # Step 2: Import necessary libraries
 import streamlit as st
